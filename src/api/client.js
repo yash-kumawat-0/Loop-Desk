563c77b95a2511e4f3df8aa1e30e8da409ec3deb
yash-kumawat-0 <kumawatyash378@gmail.com>
2026-07-02 12:27:51 +0530

    Fix: Add missing podId to LemmaClient instantiation to fix CORS error

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,6 @@
 import { LemmaClient } from "lemma-sdk";
 
-const client = new LemmaClient();
+const client = new LemmaClient({ podId: import.meta.env.VITE_LEMMA_POD_ID });
 
 export const clientReady = client.initialize();
 export default client;
